cc827023df8da5bb1a077d4bb2ee9c1510d40554
MrKevinWeiss <weiss.kevin604@gmail.com>
2021-05-18 15:22:46 +0200

    jobs: update and refactor
    - Allow prs to be rebased to master
    - Capture and display build results
    - Use common groovy script

--- job_scripts/flash_check.groovy
+++ job_scripts/flash_check.groovy
@@ -1,15 +1,15 @@
-nodes = nodesByLabel('HIL')
+TEST = "tests/sched_testing"
 
 pipeline {
     agent { label 'master' }
     options {
         // If the whole process takes more than x hours then exit
         // This must be longer since multiple jobs can be started but waiting on nodes to complete
-        timeout(time: 3, unit: 'HOURS')
+        timeout(time: 1, unit: 'HOURS')
         // Failing fast allows the nodes to be interrupted as some steps can take a while
         parallelsAlwaysFailFast()
     }
-  stages {
+    stages {
         stage('setup build server and build') {
             steps {
                 stepBuildJobs()
@@ -17,19 +17,19 @@ pipeline {
         }
         stage('node test') {
             steps {
-                runParallel items: nodes.collect { "${it}" }
+                runParallel items: nodesByLabel('HIL').collect { "${it}" }
             }
         }
     }
 
 }
 
-/* builder steps =============================================================== */
+/* riot_build steps ========================================================= */
 def stepBuildJobs() {
     script {
-        for (i = 0; i < nodes.size(); i++) {
-            board = ""
-            node (nodes[i]) {
+        for (node_name in nodesByLabel('HIL')) {
+            board = ''
+            node (node_name) {
                 board = env.BOARD
             }
             node ('riot_build') {
@@ -39,7 +39,7 @@ def stepBuildJobs() {
                     userRemoteConfigs: [[url: "https://github.com/RIOT-OS/RIOT.git",
                                         credentialsId: 'github_token']]
                 ])
-                buildJob(board, "tests/shell")
+                buildJob(board, TEST)
             }
         }
     }
@@ -59,7 +59,7 @@ def buildJob(board, test) {
     }
 }
 
-/* pi steps =============================================================== */
+/* test node steps =============================================================== */
 def runParallel(args) {
     parallel args.items.collectEntries { name -> [ "${name}": {
 
@@ -68,7 +68,7 @@ def runParallel(args) {
                 /* We want to timeout a node if it doesn't respond
                  * The timeout should only start once it is acquired
                  */
-                timeout(time: 60, unit: 'MINUTES') {
+                timeout(time: 3, unit: 'MINUTES') {
                     script {
                         stepRunNodeTests()
                     }
@@ -86,7 +86,7 @@ def stepRunNodeTests()
             def timeout_stop_exc = null
             catchError(buildResult: 'UNSTABLE', stageResult: 'FAILURE',
                     catchInterruptions: false) {
-                stepUnstashBinaries("tests/shell")
+                stepUnstashBinaries(TEST)
                 stepFlashAndTest()
             }
         }
@@ -95,12 +95,28 @@ def stepRunNodeTests()
 
 def stepUnstashBinaries(test) {
     unstash name: "${env.BOARD}_${test.replace("/", "_")}"
-    sh script: "rsync -a tests/shell/ /opt/RIOT/tests/shell/"
+    sh script: "rsync -a ${TEST}/ /opt/RIOT/${TEST}/"
 }
 
 def stepFlashAndTest()
 {
-    exit_code = sh script: "make flash-only test -C /opt/RIOT/tests/shell", returnStatus:true
+    exit_code = sh script: "make flash-only test -C /opt/RIOT/${TEST}", returnStatus:true
+    if (exit_code == 0) {
+        return
+    }
+    sh script: """
+        gpio -p mode 23 out
+        gpio -p write 23 0
+        echo '1-1' | sudo tee /sys/bus/usb/drivers/usb/unbind
+        sleep 3
+    """, returnStatus:true
+    sh script: "echo '1-1' | sudo tee /sys/bus/usb/drivers/usb/bind", returnStatus:true
+    sh script: """
+        gpio -p write 23 1
+        gpio -p mode 23 in
+        sleep 1
+    """, returnStatus:true
+    exit_code = sh script: "make flash-only test -C /opt/RIOT/${TEST}", returnStatus:true
     if (exit_code != 0) {
         sh script: "sudo reboot"
     }
